e5427da6a094aead05d7432b8961b3a6e012ef79
unknown <mrgwduncan@gmail.com>
2020-02-07 15:12:07 +1300

    cleaner

--- client/components/DrinkResults.jsx
+++ client/components/DrinkResults.jsx
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 function DrinkResults(props) {
-  //removes null items and the image field and id field
-
-  function removeUnwanted(drink) {
-    for (let propName in drink) {
-      if (drink[propName] === null || drink[propName] === undefined) {
-        delete drink[propName];
-      }
-    }
-    delete props.data.drinks[0].strDrinkThumb;
-    delete props.data.drinks[0].idDrink;
-    delete props.data.drinks[0].strCreativeCommonsConfirmed
-    delete props.data.drinks[0].dateModified
-  }
-  removeUnwanted(props.data.drinks[0]);
   return (
     <div>
-      <div className="" >
-        {/* proforms magic returns each item in the object in a p tag */}
-        {Object.keys(props.data.drinks[0]).map(function(objectKey, index) {
-          var value = props.data.drinks[0][objectKey];
-          return <p>{value}</p>;
-        })}
+      <div className="">
+        <h2>{props.name}</h2>
+        <p>{props.category}</p>
+        <p>{props.glass}</p>
+        <p>{props.instructions}</p>
+        <ul>
+          {props.ingredients.map(results => (
+            <li>
+              {results.ing} : {results.measure}
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="">
         <img src={props.image}></img>
